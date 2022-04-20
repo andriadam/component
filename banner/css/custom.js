c164821149635d0855e5b7b1ff86fc77f3fe52c5
Andri Adam <andriadam2792000@gmail.com>
2022-04-20 20:41:03 +0700

    add component banner

---
+++ banner/css/custom.js
@@ -0,0 +1,29 @@
+(function ($) {
+	
+	"use strict";
+
+
+
+	$('.owl-service-item').owlCarousel({
+		items:3,
+		loop:true,
+		dots: true,
+		nav: true,
+		autoplay: true,
+		margin:30,
+		  responsive:{
+			  0:{
+				  items:1
+			  },
+			  600:{
+				  items:2
+			  },
+			  1000:{
+				  items:3
+			  }
+		  }
+	  })
+
+	
+
+})(window.jQuery);
